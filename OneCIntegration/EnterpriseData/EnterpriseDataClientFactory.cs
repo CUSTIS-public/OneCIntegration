9d9c7f384dbb362b5f1c2cb8e75abe0b7d1fb636
Трушин П.А. <ptrushin@custis.ru>
2025-07-23 16:15:41 +0300

    -

--- OneCIntegration/EnterpriseData/EnterpriseDataClientFactory.cs
+++ OneCIntegration/EnterpriseData/EnterpriseDataClientFactory.cs
@@ -14,7 +14,12 @@ public static class EnterpriseDataClientFactory
     public static EnterpriseDataExchange_1_0_1_1PortTypeClient CreateNtlm(string serviceURL, string user, string password, string? domain = null)
     {
         var binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportCredentialOnly);
+        binding.MaxBufferSize = int.MaxValue;
+        binding.MaxReceivedMessageSize = int.MaxValue;
+        binding.ReaderQuotas.MaxArrayLength = int.MaxValue;
+        binding.ReaderQuotas.MaxStringContentLength = int.MaxValue;
         binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
+
         var client = new EnterpriseDataExchange_1_0_1_1PortTypeClient(binding, new EndpointAddress(serviceURL));
         client.ClientCredentials.Windows.ClientCredential = new NetworkCredential
         {
@@ -28,10 +33,13 @@ public static class EnterpriseDataClientFactory
     public static EnterpriseDataExchange_1_0_1_1PortTypeClient CreateBasic(string serviceURL, string user, string password)
     {
         var binding = new BasicHttpBinding(BasicHttpSecurityMode.TransportCredentialOnly);
+        binding.MaxBufferSize = int.MaxValue;
+        binding.MaxReceivedMessageSize = int.MaxValue;
+        binding.ReaderQuotas.MaxArrayLength = int.MaxValue;
+        binding.ReaderQuotas.MaxStringContentLength = int.MaxValue;
         binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Basic;
-
+        
         var client = new EnterpriseDataExchange_1_0_1_1PortTypeClient(binding, new EndpointAddress(serviceURL));
-
         client.ClientCredentials.UserName.UserName = user;
         client.ClientCredentials.UserName.Password = password;
 
